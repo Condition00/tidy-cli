0f25a74ad37e0ddc7211a872d4c1ff318041aec6
Anant Satya Mohit Kavuru <anant.kavuru@gmail.com>
2025-12-28 20:53:16 +0530

    detecting directories

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use clap::Parser;
+use clap::{Parser, Subcommand};
 use std::path::PathBuf;
 
-#[dervive(Parser)]
+#[derive(Parser)]
 #[command(name = "tidy")]
 #[command(about = "cli tool to stay organized. built in Rust")]
 
@@ -10,10 +10,28 @@ struct Cli {
     command: Commands,
 }
 
-#[dervive(clap::subcommand)]
+#[derive(Subcommand)]
 enum Commands {
     Scan {
-        #[arg(default_value_t]
-    }
+        path: Option<PathBuf>,
+    },
 }
 
+fn main() -> anyhow::Result<()> {
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::Scan { path } => {
+            let path = match path {
+                Some(p) => p,
+                None => dirs::home_dir()
+                    .ok_or_else(|| anyhow::anyhow!("could not determine home directory"))?,
+            };
+
+            let resolved_path = path.canonicalize()?;
+            println!("{}", resolved_path.display());
+        }
+    }
+
+    Ok(())
+}
