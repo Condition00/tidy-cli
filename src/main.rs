6b1badba7135c10287cb3971918ac36e9d36430c
Anant Satya Mohit Kavuru <anant.kavuru@gmail.com>
2025-12-28 20:30:42 +0530

    parser dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use std::path::PathBuf;
+
+#[dervive(Parser)]
+#[command(name = "tidy")]
+#[command(about = "cli tool to stay organized. built in Rust")]
+
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
 }
+
+#[dervive(clap::subcommand)]
+enum Commands {
+    Scan {
+        #[arg(default_value_t]
+    }
+}
+
